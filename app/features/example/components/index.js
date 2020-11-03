af41714057812eb952c2bbb6208624a3cf64fd15
Rashgaroth <putra.dwiyan26@gmail.com>
2020-11-03 16:03:34 +0700

    example init

---
+++ app/features/example/components/index.js
@@ -0,0 +1,1 @@
+// untuk menaruh component yang bersifat private artinya hanya feature ini saja yang memakai component ini
---
+++ app/features/example/containers/index.js
@@ -0,0 +1,28 @@
+// Import base component
+import React from 'react';
+import { View } from 'react-native';
+import { Button } from 'react-native-paper';
+// Import redux env
+import { useDispatch } from 'react-redux';
+import * as loginActions from 'app/features/login/actions';
+// Import style
+import styles from './styles';
+// Import Components
+
+export default function Home() {
+
+  // TODO: React Lifecycle here :
+
+
+  // TODO: Function here : 
+
+
+  // TODO: Render exiting here :
+  
+
+  return (
+    <View style={styles.container}>
+    {/* Isi dari isi si view : Button, Text, CardView, dll */}
+    </View>
+  );
+}
